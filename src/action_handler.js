41d95a7ef4369ddf7bce1fb8efea26f4d14a6295
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2023-07-12 00:19:37 -0700

    Progress on DOM loading, stuck on task creation

--- src/action_handler.js
+++ src/action_handler.js
@@ -1,9 +1,9 @@
-import {newProject} from './projects.js'
+import {createProject} from './projects.js'
 
 function initializePage() {
     loadHeader();
     loadGrid();
-    newProject();
+    createProject();
 
     return;
 }
@@ -11,17 +11,22 @@ function initializePage() {
 function loadHeader() {
     const content = document.getElementById('content');
     const header = document.createElement('div');
+    const addProj = document.createElement('div');
     const logo_box = document.createElement('div');
     const logo = document.createElement('div');
 
-    header.classList.add(..."flex justify-end border-b-4 border-white pt-4".split(' '));
+    header.classList.add(..."flex justify-between border-b-4 border-white pt-4".split(' '));
+    addProj.classList.add(..."text-white border-dotted border-l-4 border-t-4 border-r-4 p-2 ml-4".split(' '));
     logo_box.classList.add(..."w-72 bg-white rounded-t1-sm rounded-b1-sm".split(' '));
     logo.classList.add(..."text-center text-3xl font-bold".split(' '));
 
+    addProj.innerText = 'add project'
     logo.innerText = 'Simple. Tasks';
+
+    addProj.addEventListener('click', createProject);
     
     content.append(header);
-    header.append(logo_box);
+    header.append(addProj, logo_box);
     logo_box.append(logo);
 
     return;
@@ -31,13 +36,14 @@ function loadGrid() {
     const content = document.getElementById('content');
     const grid = document.createElement('div');
     
-    grid.classList.add('grid', 'grid-cols-auto', 'gap-4', 'p-6');
+    grid.classList.add(..."grid grid-cols-auto gap-4 p-6".split(' '));
     grid.id = 'grid';
 
     content.append(grid);
 
     return;
 }
+
 
 
 export {initializePage}
--- src/projects.js
+++ src/projects.js
@@ -1,6 +1,20 @@
 import add_svg from './add.svg';
 
-function newProject() {
+// const projectsArray = [];
+
+// const projectFactory = function(title) {
+//     return {title}
+// };
+
+// function createProject() {
+//     const newProject = projectFactory('new project');
+//     pushProject();
+
+//     return;
+// }
+
+
+function createProject() {
     const grid = document.getElementById('grid');
     const proj = document.createElement('div');
     const proj_header = document.createElement('div');
@@ -11,26 +25,49 @@ function newProject() {
     addTab.src = add_svg;
 
     proj.classList.add(..."w-full h-96 border-4 border-white flex flex-col gap-3".split(' '));
-    proj_header.classList.add(..."flex justify-end".split(' '));
+    proj_header.classList.add(..."flex justify-between".split(' '));
     tab_container.classList.add(..."flex flex-col gap-2".split(' '));
+    addTab.classList.add(..."border-dotted border-l-2 border-b-2 border-r-2 p-1 ml-4".split(' '));
     proj_title.classList.add(..."w-3/4 h-12 border-l-4 border-b-4 border-dotted border-white rounded-br-md".split(' '));
-    addTab.classList.add(..."border-dotted border-2 p-1 ml-3".split(' '));
 
     grid.append(proj);
-    proj.append(proj_header, tab_container, addTab_container);
-    proj_header.append(proj_title);
+    proj.append(proj_header, tab_container);
+    proj_header.append(addTab_container, proj_title);
     addTab_container.append(addTab);
 
+    proj_title.addEventListener('click', Clicks.proj_titleClick);
     addTab.addEventListener('click', Clicks.addTabClick);
     
     return;
 }
 
 const Clicks = {
+    proj_titleClick() {
+
+    },
+
     addTabClick() {
-        // come back to this, need to somehow store projects in array first
-        // so that they can be referenced.
-    }
+        createTab();
+        return;
+    },
+
+    tabClick() {
+
+    },
 };
 
-export {newProject}
+function createTab() {
+    const tab = document.createElement('div');
+    const tab_container = document.getElementById('tab_container');
+
+    tab.classList.add(..."w-11/12 h-9 bg-white rounded-br-lg rounded-tr-lg".split(' '));
+
+    tab_container.append(tab);
+
+    // issue, how do i reference the project that the evenlistener is activated in?
+    // do i need to change how projects are created?
+
+    return;
+}
+
+export {createProject}
